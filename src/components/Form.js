73602f50cf9dedd19436ea7bd834499ae2bc4c93
Utsav1510 <jiutsav8@gmail.com>
2022-10-11 11:25:34 +0530

    Delivery Project

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Cricketers from './Cricketers';
+import './Form.css';
+import { Link, useNavigate } from 'react-router-dom';
+import cricketer from './cricketer.jpg';
+import cup from './cup.png'
+
+
+export default function Form() {
+  const [position, setposition] = useState('');
+  const [player, setplayer] = useState('');
+  const [rating, setrating] = useState('');
+
+  let history = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let a = position, b = player, c = rating;
+    Cricketers.push({ a, b, c });
+    // console.log(Cricketers);
+    history('/added');
+
+
+  }
+
+  return (
+    <div>
+      <h1>Shipping Details</h1>
+      <form className='container'>
+        <label>Name</label><br></br>
+        <input type="text"   placeholder='Enter Full Name' onChange={(e) => setposition(e.target.value)} required></input><br></br>
+        <label>Contact</label><br></br>
+        <input type="number"   placeholder='Enter Mobile Number' onChange={(e) => setplayer(e.target.value)} required></input><br></br>
+        <label >Shipping Address</label><br></br>
+        <input type="text" placeholder='Enter Address' onChange={(e) => setrating(e.target.value)} required></input><br></br>
+        <Link to='/added'>
+          <input className='btn' type="submit" onClick={handleSubmit}></input>
+        </Link>
+
+      </form>
+
+      
+    </div>
+  )
+}
